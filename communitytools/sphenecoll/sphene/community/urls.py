07581743a076c06c104a0595512e93ac8e1ed6a1
dave <dave@bbad15d5-7c10-0410-ba9e-f711d5c1d17a>
2007-05-27 20:19:03 +0000

    Start using an externals link for communitytools instead of a local patched copy. Update settings.py to use the 0.96 compatibility mode in the svn version of communitytools.
    git-svn-id: https://svn.boostpro.com/clients/boostcon/site/code/trunk@2180 bbad15d5-7c10-0410-ba9e-f711d5c1d17a

--- communitytools/sphenecoll/sphene/community/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-
-urlpatterns = patterns('',
-                       (r'captcha/(?P<token_id>\w+).jpg$', 'sphene.community.views.captcha_image'),
-                       )
