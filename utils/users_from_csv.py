6fa7d2a42b0e59e1ae96b9b80aa4831228671d8e
dave <dave@bbad15d5-7c10-0410-ba9e-f711d5c1d17a>
2007-05-28 23:02:35 +0000

    Everything is ready to issue invitations to the 2007 participants.  Here goes...
    git-svn-id: https://svn.boostpro.com/clients/boostcon/site/code/trunk@2193 bbad15d5-7c10-0410-ba9e-f711d5c1d17a

--- utils/users_from_csv.py
+++ utils/users_from_csv.py
@@ -1,205 +1,80 @@
 #!/usr/bin/env python
-## Generate a human readable 'random' password
-## password  will be generated in the form 'word'+digits+'word' 
-## eg.,nice137pass
-## parameters: number of 'characters' , number of 'digits'
-## Pradeep Kishore Gowda <pradeep at btbytes.com >
-## License : GPL 
-## Date : 2005.April.15
-## Revision 1.2 
-## ChangeLog: 
-## 1.1 - fixed typos 
-## 1.2 - renamed functions _apart & _npart to a_part & n_part as zope does not allow functions to 
-## start with _
+# Issue registration invitations to the users listed in the given .csv file.
+import os, sys
+sys.path+= ['..', '../..']
+os.environ['DJANGO_SETTINGS_MODULE'] = 'boost_consulting.settings'
 
-from trac.env import Environment
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models import Q
+# from django.template.context import Context
+from django.http import HttpRequest
+from django.template import loader, Context
+from django.core.mail import send_mail
 
-def nicepass(alpha=6,numeric=2):
-    """
-    returns a human-readble password (say rol86din instead of 
-    a difficult to remember K8Yn9muL ) 
-    """
-    import string
-    import random
-    vowels = ['a','e','i','o','u']
-    consonants = [a for a in string.ascii_lowercase if a not in vowels]
-    digits = string.digits
-    
-    ####utility functions
-    def a_part(slen):
-        ret = ''
-        for i in range(slen):			
-            if i%2 ==0:
-                randid = random.randint(0,20) #number of consonants
-                ret += consonants[randid]
-            else:
-                randid = random.randint(0,4) #number of vowels
-                ret += vowels[randid]
-        return ret
-    
-    def n_part(slen):
-        ret = ''
-        for i in range(slen):
-            randid = random.randint(0,9) #number of digits
-            ret += digits[randid]
-        return ret
-        
-    #### 	
-    fpl = alpha/2		
-    if alpha % 2 :
-        fpl = int(alpha/2) + 1 					
-    lpl = alpha - fpl	
-    
-    start = a_part(fpl)
-    mid = n_part(numeric)
-    end = a_part(lpl)
-    
-    return "%s%s%s" % (start,mid,end)
+from sphene.contrib.libs.common.utils.misc import cryptString, decryptString
 
 import csv
 
 class MissingHeading(Exception):
     pass
 
-def names_and_emails(filename):
+def parse_registrations(filename):
     r = csv.reader(open(filename))
     headings = [x.lower() for x in r.next()]
-    first = headings.index('first')
-    if first < 0:
-        raise MissingHeading, 'first'
-    last = headings.index('last')
-    if last < 0:
-        raise MissingHeading, 'last'
-    email = headings.index('email')
-    if email < 0:
-        raise MissingHeading, 'email'
+    for x in r:
+        yield dict(zip(headings,x))
     
-    return [ x[first],x[last],x[email] for x in r ]
-
-import os
-
-def system(cmd):
-    print cmd
-    os.system(cmd)
-    
-def permission(args):
-    system('trac-admin /usr/local/boost/share/tracs/boostcon permission '+args)
+import re
+email_address_re = re.compile(r'[^<]*<([^>]+@[^>]+)>\s*')
 
 if __name__ == "__main__":
-    tracenv = Environment('/usr/local/share/tracs/boostcon')
-    
-    pass_file = open('/usr/local/etc/boostcon/trac-passwd', 'a+b')
-    pass_file.seek(0)
 
-    old_passwds = pass_file.readlines()
-    old_logins = set( [ l[:l.find(':')] for l in old_passwds ] )
+     for vitals in parse_registrations(sys.argv[1]):
 
-    db = tracenv.get_db_cnx()
-    known_emails = set()
-    for username,name,email in tracenv.get_known_users(db):
-        if '@' in username:
-            known_emails.add(username)
-        if '@' in email:
-            known_emails.add(email)
-            
-    ne = names_and_emails('/tmp/BoostReg.csv')
-
-    new_logins = {}
-
-    new_trac_session_attrs = []
-    new_trac_perms = []
-    for first,last,email in ne:
-        if not email in known_emails:
-            new_trac_session_attrs += [
-                (email, 1, 'name', first + ' ' + last),
-                (email, 1, 'email', email)
-                ]
-            
-        if not email in old_logins and not email in new_logins:
-            new_logins[email] = nicepass()
-        new_trac_perms.append((email,'attendee_2006'))
-
-    new_passwds = ['%s:%s' % (l, crypt.crypt(p)) for l,p in new_logins]
-
-    #
-    # Compose emails
-    #
-    message = """From: BoostCon Organizers <boostcon-plan@lists.boost-consulting.com>
-To: %(first)s %(last)s <%(email)s>
-Subject: Photos, wiki, and updated slides at http://boostcon.com
-
-Dear %(first)s %(last)s,
-
-Thanks for your participation in BoostCon'07!  We've just
-launched a community portal with:
-
-* a Photo gallery where you can upload your portrait and snapshots
-  from the event.
-
-* a Wiki where you can collaborate with other attendees
-  
-* a private Wiki area where you can download updated slide sets,
-  and you can post anything that you only want seen by other
-  BoostCon'07 attendees.
-
-* a set of discussion forums (http://boostcon.com/traq/discussion)
-  
-where you can upload photos
-"""
-
-    #
-    # Put these users on staff in Django and give them permission to add photos
-    #
-    new_django_users = []
-    old_django_users = User.objects.all()
-    from django.db.models import Q
-    for first,last,email in ne:
-        matches = old_django_users.filter(
-            Q(email = email)
-            | Q(email__endswith = '<%s>'%email)
-            | Q(username = email))
+        m = email_address_re.match(vitals['email'])
+        if (m):
+            address = m.groups(1)
+            vitals['email'] = address
+        else:
+            address = vitals['email']
         
-        if matches.count() == 0:
-            matches = [
-                User(
-                    username=email, is_staff=True, is_active=False,
-                    is_superuser=False) ] 
+        # if the email is unknown...
+        if User.objects.filter(
+          Q( email__exact = address ) | Q( email__endswith = '<%s>'%address)
+        ).count() == 0:
+            
+            validationcode = cryptString(
+                settings.SECRET_KEY,
+                '&'.join([
+                    'first_name=%(first)r',
+                    'last_name=%(last)r',
+                    'state=%(jurisdiction)r',
+                    'zip=%(postal code)r',
+                    'groups=list(Group.objects.filter(Q(name__exact="2007 Attendees")%s%s))'
+                    % (('s' in vitals['role']) and '|Q(name__exact="2007 Speakers")' or '',
+                       ('o' in vitals['role']) and '|Q(name__exact="2007 Organizers")' or ''
+                            ),
+                    'is_staff=True', # Need to be "on staff" so they can add photos :(
+                    ] + [
+                        '%s=%%(%s)r' % (x,x) for x in
+                        'email','address1','address2','affiliation','country','title','phone','fax'
+                        ])
+                % vitals
+                )
+            
+            t = loader.get_template('registration/invitation_email.txt')
+            
+            send_mail(
+                'Invitation to BoostCon community portal',
+                t.render(
+                    Context(
+                        { 'validationcode': validationcode, 'firstname':vitals['first'], }
+                        )),
+                
+                'BoostCon Organizers <boostcon-plan@lists.boost-consulting.com>',
+                [address]
+                )
 
-        for u in matches:
-            u.is_staff = True
-            u.user_permissions.add('gallery.add_photo')
-            new_django_users.append(u)
-
-    #
-    # Start committing things.  Hopefully by now we've found any crucial errors.
-    #
-    
-    #
-    # update the trac session database with usernames and emails
-    #
-    cursor = db.cursor()
-    cursor.executemany("INSERT INTO session_attribute VALUES (%s,%s,%s,%s)",
-                       new_trac_session_attrs)
-    cursor.executemany("INSERT INTO permission VALUES (%s, %s)",
-                       new_trac_perms)
-    
-    #
-    # update the passwd file 
-    #
-    pass_file.seek(0,2)
-    
-    # add a trailing newline if necessary
-    if old_passwds[-1][-1] != '\n':
-        pass_file.write('\n')
-        
-    pass_file.write('\n'.join(new_passwds))
-
-    # save django users
-    for u in new_django_users: u.save()
-    
-    #
-    # Send email notifications
-    #
     
     
