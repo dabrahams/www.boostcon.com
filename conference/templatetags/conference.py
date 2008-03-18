5d814276afa28be430d46adb56eb7d872d8ed5f5
dave <dave@bbad15d5-7c10-0410-ba9e-f711d5c1d17a>
2008-03-18 15:47:10 +0000

    Defensive hack to work around problem and get sites up again
    git-svn-id: https://svn.boostpro.com/clients/boostcon/site/code/trunk@2849 bbad15d5-7c10-0410-ba9e-f711d5c1d17a

--- conference/templatetags/conference.py
+++ conference/templatetags/conference.py
@@ -10,4 +10,12 @@ def render_rst(rst,args):
     from utils.restructuredtext import html_parts
     if args != '':
         args=','+str(args)
-    return eval('html_parts(rst.decode("utf-8")%s)' % args)['body']
+
+    # This is a defensive programming hack.  We clearly didn't know whether we
+    # were getting unicode or utf-8 strs before, so this makes sure we're in
+    # unicode by the time we call html_parts without trying to do a decode on a
+    # unicode string.  We need to figure out what's really going on here.
+    if not isinstance(rst,unicode):
+        rst = rst.decode('utf-8')
+        
+    return eval('html_parts(rst%s)' % args)['body']
