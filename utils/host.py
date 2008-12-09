33c793d89934f4eee4aa524f9c08f40d11aaaa10
jim.porter <jim.porter@bbad15d5-7c10-0410-ba9e-f711d5c1d17a>
2008-12-09 23:45:44 +0000

    Fix hostname() overloading issue (because there is no overloading in python!)
    git-svn-id: https://svn.boostpro.com/clients/boostcon/site/code/branches/django1.0@3268 bbad15d5-7c10-0410-ba9e-f711d5c1d17a

--- utils/host.py
+++ utils/host.py
@@ -3,16 +3,16 @@
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 from boost_consulting import settings
 
-def hostname(request):
-    server_name = request.META['SERVER_NAME']
-    server_port = request.META['SERVER_PORT']
-    if not server_port or server_port == '80':
-        return server_name
+def hostname(request = None):
+    if request == None:
+        try:
+            return settings.hostname
+        except AttributeError:
+            return 'localhost:8000'
     else:
-        return server_name+':'+server_port
-
-def hostname():
-    try:
-        return settings.hostname
-    except AttributeError:
-        return "localhost:8000"
+        server_name = request.META['SERVER_NAME']
+        server_port = request.META['SERVER_PORT']
+        if not server_port or server_port == '80':
+            return server_name
+        else:
+            return server_name+':'+server_port
